30c32acb7fe637a29aa156d3510af96ffd4a4daa
Alex Hentschel <alex.hentschel@flowfoundation.org>
2025-11-11 14:19:43 -0800

    minor extension of initial test setup: • including LED utils • cleanup

---
+++ src/LedUtils.cpp
@@ -0,0 +1,84 @@
+#include "LedUtils.h"
+
+// CLASS LEDExpiringToggler
+//
+// This class toggles a GPIO Pin (uint8_t for ESP32 - style GPIOx) on and off
+// throughout a specified interval. Each time toggleLED() is called, the internal
+// time reference `lastTriggerObservedMilli` is set to the current time milliseconds.
+// Throughout the interval `intervalMs` thereafter, the LED is allowed to be toggled
+// between on and off and when exceeding the interval, the LED is turned off.
+// To produce human-visible blinking, a `toggleIntervalMs` specifies the number of
+// milliseconds after which the output is alternated between on <-> off.
+//
+// This is intended to run on the controller loop, consuming minimal resources.
+// Results should be largely deterministic across different controllers as we
+// don't rely on CPU frequency
+
+const bool LEDExpiringToggler::HIGH_IS_ON = true;
+const bool LEDExpiringToggler::LOW_IS_ON = false;
+
+// constructor:
+LEDExpiringToggler::LEDExpiringToggler(uint8_t pin, unsigned long lifetimeMs, unsigned long toggleIntervalMs, bool highIsOn)
+    : pin(pin), lifetimeMs(lifetimeMs), toggleIntervalMs(toggleIntervalMs), lastTriggerObservedMilli(0), highIsOn(highIsOn) {
+  pinMode(pin, OUTPUT);
+  setLedOff();
+}
+
+void LEDExpiringToggler::toggleLED() {
+  if (expired) return;
+  unsigned long currentMillis = millis();
+  unsigned long sinceTrigger = currentMillis - lastTriggerObservedMilli;
+
+  if (sinceTrigger > lifetimeMs) { // lifetime expired
+    setLedOff();
+    expired = true;
+    return;
+  }
+
+  // still active within lifetime:
+  // We divide `sinceTrigger` by toggleIntervalMs, which is essentially a zero-based counter of
+  // the elapsed toggle intervals. We always start with HIGH, on the zero value of the counter.
+  // Taking module 2 of the counter yields 0 for even or 1 for odd values. Per convention, we start
+  // with HIGH on the always start with HIGH during the immediate toggleIntervalMs following a
+  // trigger -- corresponding to counter value 0.
+  unsigned long counter = sinceTrigger / toggleIntervalMs;
+  // modulo 2 is implemented below as bitwise AND with 1. We average the fact that the least
+  // significant bit of a binary number determines if it is even (0) or odd (1).
+  if ((counter & 1) == 0) {
+    setLedOn();
+  } else {
+    setLedOff();
+  }
+}
+
+void LEDExpiringToggler::trigger() {
+  if (lifetimeMs == 0) return; // no lifetime, so we don't need to trigger
+  lastTriggerObservedMilli = millis();
+  expired = false;
+  setLedOn();
+}
+
+void LEDExpiringToggler::expire() {
+  expired = true;
+  setLedOff();
+}
+
+bool LEDExpiringToggler::isExpired() {
+  return expired;
+}
+
+void LEDExpiringToggler::setLedOn() {
+  if (highIsOn) {
+    digitalWrite(pin, HIGH);
+  } else {
+    digitalWrite(pin, LOW);
+  }
+}
+
+void LEDExpiringToggler::setLedOff() {
+  if (highIsOn) {
+    digitalWrite(pin, LOW);
+  } else {
+    digitalWrite(pin, HIGH);
+  }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,19 @@
 #include <Arduino.h>
+#include <U8g2lib.h>
+
+// WIFI
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
 
-#include <U8g2lib.h>
+// custom utils
+#include "LedUtils.h"
 
-#undef LED_BUILTIN
-#define LED_BUILTIN 8
+/* ▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅ System CONFIGURATION ▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅ */
+// Wifi credentials:
+#include "WiFiCredentials.h"
 
-// OLED 72x40 screen is
+/* On-Board Screen (OLED 72x40)
+ * ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ */
 
 U8G2_SSD1306_72X40_ER_F_SW_I2C u8g2(U8G2_R2, 6, 5, U8X8_PIN_NONE);
 // U8G2_R0 	No rotation, landscape
@@ -22,18 +28,29 @@ int yOffset = 24; // = (64-h)/2
 
 const char DEG_SYM[] = {0xB0, '\0'};
 
-u8g2_uint_t offset1, offset2; // current offset for the scrolling text
-u8g2_uint_t width1, width2;   // pixel width of the scrolling text (must be lesser than 128 unless U8G2_16BIT is defined
 const unsigned int text1_y0 = 34, text2_y0 = 66;
-const char *text1 = "Radio Chaine 3 ";             // scroll this text from right to left
-const char *text2 = "Katy Perry - Miss You More "; // scroll this text from right to left
+const char *text1 = "Bunny Happyness ";             // scroll this text from right to left
+const char *text2 = "The Cat Sleeps well tonight "; // scroll this text from right to left
 
-/* ▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅ CONTROLLER CONFIGURATION ▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅ */
+/* LEDs
+ * ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ */
+#define BLUE_LED_BUILTIN 8 // GPIO 8, Blue LED: LOW = on, HIGH = off
 
-// WIFI
-#include "WiFiCredentials.h"
+// LED Blinking patterns to indicate current state
+LEDExpiringToggler *blueToggler = nullptr; // blinks 5 times turning o1 second
 
-#define LED_BUILTIN 8 // Blue; LOW = on, HIGH = off
+/* Controller Output for External Load -> GPIO
+ * ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ */
+// EXT_LOAD_SWITCH defines the GPIO that is used to controll an external load attached.
+// EXT_LOAD_ON and EXT_LOAD_OFF define the states that correspond to the load being provided
+// power or not. Here, we use the Solid State Relay [SSR] H3MB-052D from Ingenex, which
+// connects its load pins on input HIGH
+#define EXT_LOAD_SWITCH 1 // GPIO 1 controls the external load (through a Mosfet supplying 5V trigger to SSR switching AC mains)
+#define EXT_LOAD_ON HIGH
+#define EXT_LOAD_OFF LOW
+
+// For testing purposes, we are "misusing" an LED toggler to control the external load logic
+LEDExpiringToggler *extLoadToggler = nullptr;
 
 /* ▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅ CONTROLLER INITIALIZATION ▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅ */
 
@@ -45,32 +62,32 @@ const char *text2 = "Katy Perry - Miss You More "; // scroll this text from righ
 
 void setup() {
   Serial.begin(115200);
-  pinMode(LED_BUILTIN, OUTPUT);
-  delay(500);
-  Serial.println("Hello ESP32-C3!!");
-
-  // u8x8.begin();
-  // u8x8.setPowerSave(0);
-  // u8x8.setFont(u8x8_font_chroma48medium8_r);
-
   u8g2.begin();
   u8g2.clearBuffer();
   u8g2.setContrast(1);      // set contrast to maximum
   u8g2.setBusClock(400000); // 400kHz I2C
 
-  // see https://github.com/olikraus/u8g2/issues/329
-
-  // u8g2.setFont(u8g2_font_ncenB14_tr); // choose a suitable font
-  // u8g2.setFont(u8g2_font_inb30_mr); // set the target font to calculate the pixel width
   u8g2.enableUTF8Print();
   u8g2.setFont(u8g2_font_logisoso30_tf); // set the target font to calculate the pixel width
-  // u8g2.setFont(u8g2_font_ncenB10_tr);
+  u8g2.setFontMode(0);                   // enable transparent mode, which is faster
 
-  width1 = u8g2.getUTF8Width(text1); // calculate the pixel width of the text
-  width2 = u8g2.getUTF8Width(text2); // calculate the pixel width of the text
-  u8g2.setFontMode(0);               // enable transparent mode, which is faster
-  offset1 = 0;                       // start over again
-  offset2 = 0;                       // start over again
+  blueToggler = new LEDExpiringToggler(BLUE_LED_BUILTIN, 1350, 150, LEDExpiringToggler::LOW_IS_ON); // blinks 1 times turning o1 second
+  blueToggler->trigger();
+  while (true) {
+    delay(20);
+    blueToggler->toggleLED();
+    if (blueToggler->isExpired()) break;
+  }
+
+  /* ── LEDs' blinking patterns to indicate current state ─────────── */
+  blueToggler = new LEDExpiringToggler(BLUE_LED_BUILTIN, 999999999, 2000, LEDExpiringToggler::LOW_IS_ON); // blinks 1 times turning o1 second
+
+  /* ── Toggling GPIO 1, which connects to Mosfet ─────────── */
+  extLoadToggler = new LEDExpiringToggler(EXT_LOAD_SWITCH, 999999999, 2000, LEDExpiringToggler::HIGH_IS_ON); // blinks 1 times turning o1 second
+
+  blueToggler->trigger();
+  extLoadToggler->trigger();
+  Serial.println("ESP32-C3 woken up!");
 }
 
 /* ▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅ CONTROLLER LOOP ▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅ */
@@ -78,43 +95,18 @@ void setup() {
  * ╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴ */
 
 void loop() {
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(50);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(2000);
-  Serial.println("ESP32-C4 Loop!!");
-
-  // u8g2.firstPage();
-  // do {
-  //   u8g2.setCursor(33, 29);
-  //   u8g2.print("24-02-种");
-  //   u8g2.setCursor(33, 46);
-  //   u8g2.print("23:55:器");
-  //   u8g2.drawFrame(30, 12, 72, 40);
-  // } while (u8g2.nextPage());
-
   u8g2.clearBuffer();                  // clear the internal memory
   u8g2.drawFrame(0, 0, width, height); // draw a frame around the border
   u8g2.setCursor(xOffset + 15, yOffset + 25);
   // u8g2.printf("%dx%d", width, height);
-  u8g2.drawUTF8(0, text1_y0, "21"); // draw the scolling text
+  u8g2.drawUTF8(0, text1_y0, "12"); // draw the scolling text
 
   u8g2.drawUTF8(42, text1_y0 + 6, "°"); // draw the scolling text
   u8g2.drawUTF8(54, text1_y0, "C");
-
-  // u8g2.drawUTF8(47, text1_y0, DEG_SYM); // prints degree symbol u8g2. drawStr (54, 17, "C");
-
-  // u8g2.drawGlyph(47, text1_y0, 176);
-  //  u8g2.print(" °C");
-
   u8g2.sendBuffer(); // transfer internal memory to the display
 
-  // u8x8.drawString(0, 1, "Hello w.!");
-  // u8x8.setInverseFont(1);
-  // u8x8.drawString(0, 0, "012345678");
-  // u8x8.setInverseFont(0);
-  // u8x8.refreshDisplay();
-  delay(2000);
+  blueToggler->toggleLED();
+  extLoadToggler->toggleLED();
 }
 
 /* ▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅ BUSINESS LOGIC FUNCTIONS ▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅ */
